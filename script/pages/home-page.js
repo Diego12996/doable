54f8b040b7c499d869990a8e45bc3feec9ae6918
Diego Manrique <diego.manrique.m@uni.pe>
2022-06-04 04:01:38 -0500

    Add Logout button for home page

--- script/pages/home-page.js
+++ script/pages/home-page.js
@@ -1,17 +1,42 @@
+import DOMHandler from "../dom-handler.js"
+import { logout } from "../services/session-services.js"
 import STORE from "../store.js"
+import LoginPage from "./login-page.js"
 
 function render() {
-  console.log(STORE)
   return `
-    <h1>Home Page</h1>
+  <main class="section">
+    <section class="container">
+      <h1 class="heading heading--lg text-center mb-2">Expensable</h1>
+      <a class="text-center block mb-8 js-logout" href="#" data-action="logout">Logout</a>
+    </section>
+  </main>
   `
+}
+
+function listenLogout() {
+  const logoutLink = document.querySelector('[data-action="logout"]')
+  logoutLink.addEventListener("click", async (event) => {
+    event.preventDefault()
+    // DOMHandler.reload();
+
+    try {
+      await logout()
+      DOMHandler.load(LoginPage)
+    } catch(error) {
+      console.log(error.message)
+      DOMHandler.reload()
+    }
+  })
 }
 
 const HomePage = {
   toString() {
     return render()
   },
-  addListeners() {}
+  addListeners() {
+    listenLogout();
+  }
 }
 
 export default HomePage;
